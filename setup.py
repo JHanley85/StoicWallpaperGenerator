9cd226e9d6e8ec4668e311b0edde4a6d6779c85d
Mir <hanneswartel@gmail.com>
2020-11-17 16:06:04 +0100

    Version one done!

--- setup.py
+++ setup.py
@@ -1,14 +1,50 @@
 from cx_Freeze import setup, Executable
 
+
 build_exe_options = {"packages": ["autoMeditationsWallpaper"], "excludes": ["asyncio", "email", "html", "http",
                                                                             "lib2to3", "multiprocessing", "pydoc_data",
                                                                             "test", "tkinter", "unittest", "xmlrpc"]}
-executables = [Executable("runAMW.py", icon="logo.ico")]
+
+shortcut_table = [
+    ("DesktopShortcut",        # Shortcut
+     "DesktopFolder",          # Directory_
+     "program",           # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]stoic_wallpaper_generator.exe",# Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     "icon.ico",                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     ),
+
+     ("StartupShortcut",        # Shortcut
+     "StartupFolder",          # Directory_
+     "program",           # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]stoic_wallpaper_generator.exe",# Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     "logo.ico",                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     ),
+    ]
+
+
+executables = [Executable("stoic_wallpaper_generator.py", icon="logo.ico", shortcutName='stoic_wallpaper_generator', shortcutDir='DesktopFolder')]
+msi_data = {"Shortcut": shortcut_table}
+bdist_msi_options = {'data': msi_data}
 
 setup(
     name="stoic_wallpaper_generator",
     version="1",
     description="cx_Freeze script to test pillow (PIL)",
     executables=executables,
-    options={"build_exe": build_exe_options},
+    options={"build_exe": build_exe_options, "bdist_msi": bdist_msi_options
+        },
 )
